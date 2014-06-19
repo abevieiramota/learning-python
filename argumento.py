0188f08e79b7a772650bd59b759c91c1d2b2adea
abevieiramota <abevieiamota@gmail.com>
2014-06-19 00:25:59 -0300

    até lição 16 do hardway

---
+++ argumento.py
@@ -0,0 +1,8 @@
+from sys import argv
+
+script, first, second, third = argv
+
+print 'script chamado', script
+print 'primeiro argumento', first
+print 'segundo argumento', second
+print 'terceiro argumento', third
