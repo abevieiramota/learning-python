7e196d2cbacbdfa61a5b7cf35771340e5cadfd1a
Abelardo Vieira Mota <abevieiramota@gmail.com>
2014-07-10 13:01:21 -0300

    finalizado ch04

--- python_3_object_oriented_programming/ch04/exception.py
+++ python_3_object_oriented_programming/ch04/exception.py
@@ -1,1 +1,12 @@
+class EvenOnly(list):
+    def append(self, integer):
+        if not isinstance(integer, int):
+            raise TypeError("Only integers can be added")
+        if integer % 2:
+            raise ValueError("Only even numbers can be added")
+        super(EvenOnly, self).append(integer)
 
+def no_return():
+    print 'entrou'
+    raise Exception("sempre lança")
+    print 'nunca printa'
